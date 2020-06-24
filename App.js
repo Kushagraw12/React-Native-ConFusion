b94c82f6a6aa91be766a0995dc09daafcd2afc3a
Kushagra <kush12092001@gmail.com>
2020-06-24 20:59:35 +0530

    Components added

---
+++ App.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+{
+  /*import { Platform, StyleSheet, Text, View } from "react-native";*/
+}
+import Main from "./components/MainComponent";
+
+export default function App() {
+  return <Main />;
+}
+{
+  /*
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#0ff",
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5,
+  },
+});
+*/
+}
