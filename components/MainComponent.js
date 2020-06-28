fd6955e77f309bf4123061f1ecbccf54c61450b2
Kushagra <kush12092001@gmail.com>
2020-06-28 18:50:10 +0530

    Navigation-Stack added

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,7 +2,35 @@ import React, { Component } from "react";
 import Menu from "./MenuComponent";
 import { DISHES } from "../shared/dishes";
 import Dishdetail from "./DishdetailComponent";
-import { View } from "react-native";
+import { View, Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const MenuNavigator = createStackNavigator();
+
+function MenuNavigatorScreen() {
+  return (
+    <MenuNavigator.Navigator
+      initialRouteName="Menu"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#512DA8",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          color: "#fff",
+        },
+      }}
+    >
+      <MenuNavigator.Screen name="Menu" component={Menu} />
+      <MenuNavigator.Screen
+        name="Dishdetail"
+        component={Dishdetail}
+        options={{ headerTitle: "Dish Detail" }}
+      />
+    </MenuNavigator.Navigator>
+  );
+}
 
 class Main extends Component {
   constructor(props) {
@@ -19,19 +47,9 @@ class Main extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <Menu
-          dishes={this.state.dishes}
-          onPress={(dishId) => this.onDishSelect(dishId)}
-        />
-        <Dishdetail
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
-      </View>
+      <NavigationContainer>
+        <MenuNavigatorScreen />
+      </NavigationContainer>
     );
   }
 }
